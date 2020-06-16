577b0953df4a6cfd1609d8fb20d110856f8092fb
Anton Firszov <antonfir@gmail.com>
2020-06-16 02:22:05 +0200

    DelayedAck

---
+++ DelayedAck/DelayedAck-Client/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace DelayedAck_Client
+{
+    internal class Program
+    {
+        private const int N = 100000;
+        
+        public static async Task Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Address please!");
+                return;
+            }
+
+            IPEndPoint serverEndPoint = IPEndPoint.Parse(args[0]);
+            
+            using Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            await client.ConnectAsync(serverEndPoint);
+            
+            // Warm up:
+            byte[] buffer = new byte[1];
+            await client.SendAsync(buffer, SocketFlags.None);
+            await client.ReceiveAsync(buffer, SocketFlags.None);
+
+
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            for (int i = 0; i < N; i++)
+            {
+                await client.SendAsync(buffer, SocketFlags.None);
+                await client.ReceiveAsync(buffer, SocketFlags.None);
+            }
+            
+            stopwatch.Stop();
+
+            TimeSpan averageDelay = stopwatch.Elapsed / (2 * N);
+            Console.WriteLine(averageDelay);
+        }
+    }
+}
---
+++ DelayedAck/DelayedAck-Server/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace DelayedAck_Server
+{
+    class Program
+    {
+        public const int N = 100000;
+        
+        static async Task Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Address please!");
+                return;
+            }
+            
+            IPEndPoint serverEndPoint = IPEndPoint.Parse(args[0]);
+            
+            using Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            listener.Bind(serverEndPoint);
+            listener.Listen(10);
+
+            Console.WriteLine("Awaiting connection ...");
+            using Socket server = await listener.AcceptAsync();
+            Console.WriteLine("OK. Testing ...");
+
+            byte[] buffer = new byte[1];
+            while(true)
+            {
+                int bytesReceived = await server.ReceiveAsync(buffer, SocketFlags.None);
+                if (bytesReceived != 1) throw new Exception("Meh");
+                await server.SendAsync(buffer, SocketFlags.None);
+            }
+        }
+    }
+}
