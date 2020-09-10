e7313ca585ab20a05baabadfce0640d8c8677b11
Anton Firszov <antonfir@gmail.com>
2020-09-10 16:42:21 +0200

    I can has WinHttp now

--- Http/WinHttpSandbox/WinHttpSandbox.cpp
+++ Http/WinHttpSandbox/WinHttpSandbox.cpp
@@ -1,11 +1,79 @@
 // WinHttpSandbox.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+#include <winhttp.h>
 #include <iostream>
+#include "Common.hpp"
+
+#pragma comment (lib, "winhttp.lib")
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    DWORD dwSize = 0;
+    DWORD dwDownloaded = 0;
+    LPSTR pszOutBuffer;
+    BOOL  bResults = FALSE;
+
+    HINTERNET hSession = NULL, hConnect = NULL, hRequest = NULL;
+
+    try {
+        // Use WinHttpOpen to obtain a session handle.
+        hSession = TRYNZ(
+            WinHttpOpen(L"Test!", WINHTTP_ACCESS_TYPE_DEFAULT_PROXY, WINHTTP_NO_PROXY_NAME, WINHTTP_NO_PROXY_BYPASS, 0)
+        );
+
+        hConnect = TRYNZ(
+            WinHttpConnect(hSession, L"motherfuckingwebsite.com", INTERNET_DEFAULT_HTTPS_PORT, 0)
+        );
+
+        hRequest = TRYNZ(
+            WinHttpOpenRequest(hConnect, L"GET", NULL, NULL, WINHTTP_NO_REFERER, WINHTTP_DEFAULT_ACCEPT_TYPES, WINHTTP_FLAG_SECURE)
+        );
+
+        TRYNZ(
+            WinHttpSendRequest(hRequest,
+                WINHTTP_NO_ADDITIONAL_HEADERS, 0,
+                WINHTTP_NO_REQUEST_DATA, 0,
+                0, 0)
+        );
+
+        TRYNZ(
+            WinHttpReceiveResponse(hRequest, NULL)
+        );
+
+        
+        std::stringstream output;
+
+        do {
+            TRYNZ(WinHttpQueryDataAvailable(hRequest, &dwSize));
+
+            DWORD bufferSize = dwSize + 1;
+
+            auto buffer = new char[bufferSize]();
+
+            DWORD dwDownloaded = 0;
+            TRYNZ(
+                WinHttpReadData(hRequest, (LPVOID)buffer, dwSize, &dwDownloaded)
+            );
+
+            output << std::string(buffer, (size_t)dwDownloaded);
+
+        } while (dwSize > 0);
+
+        std::cout << output.str() << std::endl;
+    }
+    catch (const OsError& error) {
+        std::cout << error.what() << std::endl;
+    }
+
+    if (hRequest) WinHttpCloseHandle(hRequest);
+    if (hConnect) WinHttpCloseHandle(hConnect);
+    if (hSession) WinHttpCloseHandle(hSession);
+
+    std::cout << "LOLOLOLOLOLOL" << std::endl;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
