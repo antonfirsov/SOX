4292c9943eeb6b24dc72081cc74885f71bd0e9fa
Anton Firszov <antonfir@gmail.com>
2021-01-22 16:25:42 +0100

    some junk

---
+++ BasicTCP/BasicTcpClient-dotnet/Utils.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+
+namespace Client
+{
+    internal static class Utils
+    {
+        const int SOL_SOCKET = 0xffff;
+        const int SO_KEEPALIVE = 0x0008;
+
+        private static unsafe void QueryKeepalive(Socket socket)
+        {
+            int enabled = -1;
+            void* pEnabled = &enabled;
+            socket.GetRawSocketOption(SOL_SOCKET, SO_KEEPALIVE, new Span<byte>(pEnabled, sizeof(int)));
+
+            Console.WriteLine("Enabled: "+enabled);
+        }
+
+        public static unsafe void EnableKeepaliveWindows(this Socket socket, uint time = 2000, uint interval = 500)
+        {
+            QueryKeepalive(socket);
+
+            tcp_keepalive keepalive = new tcp_keepalive
+            {
+                onoff = 1,
+                keepalivetime = time,
+                keepaliveinterval = interval
+            };
+            void* pKeepalive = &keepalive;
+
+            byte[] bytes = new Span<byte>(pKeepalive, sizeof(tcp_keepalive)).ToArray();
+            byte[] outVals = new byte[bytes.Length];
+            int retVal = socket.IOControl(IOControlCode.KeepAliveValues, bytes, outVals);
+            if (retVal != 0)
+            {
+                throw new Exception("IOControl(IOControlCode.KeepAliveValues) != 0");
+            }
+
+            QueryKeepalive(socket);
+        }
+
+        struct tcp_keepalive
+        {
+            public UInt32 onoff;
+            public UInt32 keepalivetime;
+            public UInt32 keepaliveinterval;
+        };
+    }
+}
---
+++ Other/SendReceiveCancellation-TCP/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace SendReceiveCancellation_TCP
+{
+    class Program
+    {
+        static Task Main(string[] args)
+        {
+            return Tcp();
+        }
+
+        private static async Task Tcp()
+        {
+            using Socket listener = new Socket(SocketType.Stream, ProtocolType.Tcp);
+            listener.SendBufferSize = 0;
+            IPEndPoint ep = new IPEndPoint(IPAddress.Loopback, 0);
+            listener.Bind(ep);
+            listener.Listen(1);
+            ep = (IPEndPoint)listener.LocalEndPoint;
+
+            using Socket receiver = new Socket(SocketType.Stream, ProtocolType.Tcp);
+            receiver.ReceiveBufferSize = 1000;
+
+            var connectTask = receiver.ConnectAsync(ep);
+            var acceptTask = listener.AcceptAsync();
+
+            using Socket sender = await acceptTask;
+            await connectTask;
+
+            sender.SendBufferSize = 0;          
+
+            byte[] sendData = new byte[3000];
+            byte[] receiveData = new byte[5000];
+
+            var sendTask = sender.SendAsync(sendData, SocketFlags.None);
+            var receiveTask = receiver.ReceiveAsync(receiveData, SocketFlags.None);
+
+            int received = await receiveTask;
+            Console.WriteLine($"Received: {received}");
+            int sent = await sendTask;
+            Console.WriteLine($"Sent: {sent}");
+        }
+    }
+}
