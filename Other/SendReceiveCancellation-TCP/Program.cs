5ac9942a2b744ab42a3b1efe053784e1d5156c65
Anton Firszov <antonfir@gmail.com>
2021-01-22 16:51:18 +0100

    DisposeDuringPendingReceiveFrom_UDP

--- Other/SendReceiveCancellation-TCP/Program.cs
+++ Other/SendReceiveCancellation-TCP/Program.cs
@@ -10,11 +10,44 @@ namespace SendReceiveCancellation_TCP
     {
         static async Task Main(string[] args)
         {
-            await DisposeDuringPendingReceiveAsync(true);
-            await DisposeDuringPendingReceiveAsync(false);
+            //await DisposeDuringPendingReceive_TCP(true);
+            //await DisposeDuringPendingReceive_TCP(false);
+            await DisposeDuringPendingReceiveFrom_UDP(true);
+            await DisposeDuringPendingReceiveFrom_UDP(false);
         }
 
-        private static async Task DisposeDuringPendingReceiveAsync(bool disposeOrClose)
+        private static async Task DisposeDuringPendingReceiveFrom_UDP(bool disposeOrClose)
+        {
+            using var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            socket.Bind(new IPEndPoint(IPAddress.Any, 0));
+
+            EndPoint testEndpoint = new IPEndPoint(IPAddress.Parse("1.2.3.4"), 1234);
+
+            void DoReceiveFrom()
+            {
+                try
+                {
+                    socket.ReceiveFrom(new byte[128], ref testEndpoint);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"ABORTED! {ex.GetType().Name} : {ex.Message}");
+                }
+            }
+
+            Task receiveTask = Task.Run(DoReceiveFrom);
+            await Task.Delay(200);
+            if (disposeOrClose) socket.Dispose();
+            else socket.Close();
+
+            var timeoutTask = Task.Delay(5000);
+            if (Task.WhenAny(receiveTask, timeoutTask) == timeoutTask)
+            {
+                Console.WriteLine("TIMEOUT!!!");
+            }
+        }
+
+        private static async Task DisposeDuringPendingReceive_TCP(bool disposeOrClose)
         {
             using Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             using Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
